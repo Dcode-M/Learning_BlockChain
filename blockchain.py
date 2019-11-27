5961e4c04c866d1eb2239cec0fc34d032aa5d117
mugendinjue <mugendinjue7@gmail.com>
2019-11-27 11:22:26 +0300

    adding nodes

--- blockchain.py
+++ blockchain.py
@@ -4,14 +4,22 @@ from time import time
 from uuid import uuid4
 from flask import Flask, request, jsonify
 from textwrap import dedent
-# import jasonify
+from urllib.parse import urlparse
+import requests
+
 
 
 class learn_blockchaining(object):
   def __init__(self):
+
+    # initializing an empty list that will store the chain of blocks created
     self.chain = []
 
+    # initialize an empty list that will store the current transaction created
     self.current_transaction = []
+
+    # initialize an empty set that will store our nodes
+    self.nodes = set()
 
 
     # creating a genisis block
@@ -34,7 +42,7 @@ class learn_blockchaining(object):
     }
 
     # reset the current list of transactions
-    self.current_transaction = []
+    self.current_transaction = [] 
     self.chain.append(block)
 
     return block
@@ -110,6 +118,89 @@ class learn_blockchaining(object):
     guess_hash = hashlib.sha256(guess).hexdigest()
     
     return guess_hash[:4] == '0000'
+
+
+  def register_node(self, address):
+    '''
+    Adds a new new node to the list
+    : param address : <str> Address of node
+    :return None
+    '''
+    print(address)
+    parsed_url = urlparse(address)
+    print(parsed_url)
+    self.nodes.add(parsed_url.netloc)
+
+  
+  def valid_chain(self, chain):
+    '''
+    Determines if the blockchain is valid
+    : param chain : <list> 
+    : return <bool> True if valid and False if not valid
+    '''
+
+    last_block = chain[0]
+    current_index = 1
+
+    while current_index < len(chain):
+      block = chain[current_index]
+      print(f'{last_block}')
+      print(f'{block}')
+      print('****************************')
+      
+      # check if the hash of that block is correct
+
+      if block['previous_hash'] != self.hash(last_block):
+        return False
+
+      # check if the proof of work is correct
+
+      if not self.valid_proof(last_block['proof'], block['proof']):
+        return False
+
+
+      last_block = block
+      current_index += 1
+
+    return True
+
+
+  def resolve_conflicts(self):
+    '''
+    Consensus Algorithim
+    : return : <bool> True if the chain is replaced and False otherwise
+    '''
+
+    neighbours = self.nodes
+    new_chain = None
+
+    # looking for a chain longer than ours
+
+    max_length = len(self.chain)
+
+    # grab and verify the chains for all the nodes in the network
+
+    for node in neighbours:
+      response = requests.get(f'http://{node}/chain')
+
+      if response.status_code == 200:
+        length = response.json()['length']
+        chain = response.json()['chain']
+
+        # checking if the length is longer and if the chain is valid
+
+        if length > max_length and self.valid_chain(chain):
+          max_length = length
+          new_chain = chain
+
+    # replace the chain if we find a new valid chain longer than others
+
+    if new_chain:
+      self.chain = new_chain
+      return True
+
+    return False
+
     
 
 
@@ -183,6 +274,42 @@ def full_chain():
   return jsonify(response), 200
 
 
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+  values = request.get_json()
+
+  nodes = values.get('nodes')
+  if nodes is None:
+    return 'Error : Please supply a valid list of nodes', 400
+
+  for node in nodes:
+    blockchain.register_node(node)
+
+  response = {
+    'message' : 'New nodes have been created',
+    'total_nodes' : list(blockchain.nodes),
+  }
+  return jsonify(response), 201
+
+
+  @app.route('/nodes/resolve', methods=['GET'])
+  def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+      response = {
+        'message' : 'Our chain was replaced',
+        'new_chain' : blockchain.chain
+      }
+    else:
+      response = {
+        'message' : 'Our chain is authoritative',
+        'chain' : blockchain.chain
+      }
+
+    return  jsonify(response), 200
+
+
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=5000, debug=True)
+  app.run(host='0.0.0.0', port=5001, debug=True)
